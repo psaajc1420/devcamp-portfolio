6a0c35d2ae0d535f5cf1d97cc0cb303663943e89
Jacob Cadena <psaajc1420@yahoo.com>
2018-03-16 00:33:42 -0500

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper layout_name
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
@@ -18,5 +18,44 @@ module ApplicationHelper
 
   def copyright_generator
     CViewTool::Renderer.copyright 'Jacob Cadena', 'All rights reserved'
+  end
+
+  def nav_items
+   [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolios'
+      },
+   ]
+  end
+
+  def nav_helper tag_type, link_style, tag_style=''
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type} class='#{tag_style}'><a href='#{item[:url]}' class='#{link_style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
